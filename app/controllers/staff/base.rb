eba83c322541e511b3f4090bbab80a7d0f74a23f
fullmetal248 <fullmetal248@mctk.net>
2016-04-18 16:41:53 +0900

    typoを修正

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -2,7 +2,7 @@ class Staff::Base < ApplicationController
   private
   def current_staff_member
     if session[:staff_member_id]
-      @current_session_member ||=
+      @current_staff_member ||=
           StaffMember.find_by(id: session[:staff_member_id])
     end
   end
