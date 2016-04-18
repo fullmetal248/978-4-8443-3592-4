12d19a01dce4584e97920228531d8f4446202307
fullmetal248 <fullmetal248@mctk.net>
2016-04-18 16:59:28 +0900

    adminのログインボタンとかを表示するように

---
+++ app/controllers/admin/base.rb
@@ -0,0 +1,11 @@
+class Admin::Base < ApplicationController
+  private
+  def current_admin_member
+    if session[:admin_member_id]
+      @current_admin_member ||=
+          Administrator.find_by(id: session[:admin_member_id])
+    end
+  end
+
+  helper_method :current_admin_member
+end
