703321aaa267c423f079fba9fda78b3ed7e0292d
fullmetal248 <fullmetal248@mctk.net>
2016-04-21 03:20:00 +0900

    seedのデータをちょっと変更

--- db/seeds/development/administrators.rb
+++ db/seeds/development/administrators.rb
@@ -1,9 +1,4 @@
 Administrator.create!(
                email: 'hanako@example.com',
-               family_name: '山田',
-               given_name: '花子',
-               family_name_kana: 'ヤマダ',
-               given_name_kana: 'ハナコ',
                password: 'foobar',
-               start_date: Date.today
 )
