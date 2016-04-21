e7fa4c54c09e9dff2bd311ae9379f466ea1e8ba1
fullmetal248 <fullmetal248@mctk.net>
2016-04-21 17:15:10 +0900

    staffがアカウントをいじる際のルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -81,6 +81,7 @@ Rails.application.routes.draw do
     get 'login' => 'sessions#new', as: :login
     post 'session' => 'sessions#create', as: :session
     delete 'session' => 'sessions#destroy'
+    resource :account, except: [ :new, :create, :destroy]
   end
 
   root 'errors#routing_error'
