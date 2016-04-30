bf2e0bee85e865944b33de48c24ec240a95fe026
fullmetal248 <fullmetal248@mctk.net>
2016-04-30 18:40:24 +0900

    管理者ページにタイムアウトの機能を追加

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -25,5 +25,19 @@ class Admin::Base < ApplicationController
     end
   end
 
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_administrator
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:administrator_id)
+        flash.alert = 'セッションがタイムアウトしました。'
+        redirect_to :admin_login
+      end
+    end
+  end
+
   helper_method :current_administrator
 end
